e4490c05ca0d5dc58ac36f40808811f8a216bb88
yurybv <yuriy.bv.yuriy@gmail.com>
2023-02-10 14:54:31 +0400

    add navigation and update styles with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def home(render):
-    return HttpResponse('<h1>Blog Home</h1>')
+posts = [
+    {
+        'author': 'Yury',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'January 1, 2023'
+    },
+    {
+        'author': 'Almaz',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 2, 2023'
+    }
+]
 
 
-def about(reqeuest):
-    return HttpResponse('<h1>Blog About</h1>')
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
